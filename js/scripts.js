29349af34a6f791d81358d4286abe155a3c80ba8
Pamento <pamento@op.pl>
2017-04-19 22:36:07 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Podaj wielkosc podstawy trojkata uzywajac liczbe calkowita'),
+	h = prompt('Podaj wielkosc wysokosci trojkata uzywajac liczbe calkowita'),
+	triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
