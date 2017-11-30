a7f5960d1e89213ad57d03d9d54d9a4097ac5826
Pamento <pamento.dev@gmail.com>
2017-11-30 12:05:05 +0100

    creation LP

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
-Start next repository
+Animation in the background of LP
 =======
-# zadanie_8_5
-The beginning of the path, can be difficult and very exciting in the same time.
+# Animations of LP
+Animation with css.
 >>>>>>> f9e70dbae3787cf433262fa1d0d9303e12f41e8c
--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-var a = prompt('Podaj wielkosc podstawy trojkata uzywajac liczbe calkowita'),
-	h = prompt('Podaj wielkosc wysokosci trojkata uzywajac liczbe calkowita'),
-	triangleArea = a*h/2;
-alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
